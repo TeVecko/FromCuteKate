625327866d6da06378768673ceb79e0cd99b6fa1
Tereza Vecková <te.veckova@gmail.com>
2021-08-19 15:35:23 +0200

    přidána sekce o mně, mobile first

--- my-app/src/index.jsx
+++ my-app/src/index.jsx
@@ -4,7 +4,7 @@ import './style.css';
 
 const App = () => (
   <>
-    <div className="container">
+    <section className="sectionIntroduction">
       <header>
         <img className="introductoryImg" src="./img/vetvicka.jpg"></img>
         <h1>FromCuteKate</h1>
@@ -15,13 +15,28 @@ const App = () => (
           <p>Vyrobeno s láskou, inspirováno přírodou.</p>
         </div>
         <div className="introductory">
-          <img
-            className="arrowIntroductory bounceDown"
-            src="./img/down-sketched-arrow.png"
-          ></img>
+          <a href="#sectionKata">
+            <img
+              className="arrowIntroductory bounceDown"
+              src="./img/down-sketched-arrow.png"
+            ></img>
+          </a>
         </div>
       </main>
-    </div>
+    </section>
+    <section className="sectionKata" id="sectionKata">
+      <img className="profil" src="./img/profil.jpg"></img>
+      <h1 className="aboutMe">Kdo jsem</h1>
+      <p className="textAboutMe">
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris
+        elementum mauris vitae tortor. Fusce consectetuer risus a nunc. Etiam
+        posuere lacus quis dolor. Curabitur bibendum justo non orci. Fusce
+        suscipit libero eget elit. Integer pellentesque quam vel velit. In
+        convallis. Suspendisse sagittis ultrices augue. Morbi imperdiet, mauris
+        ac auctor dictum, nisl ligula egestas nulla, et sollicitudin sem purus
+        in lacus.
+      </p>
+    </section>
   </>
 );
 
